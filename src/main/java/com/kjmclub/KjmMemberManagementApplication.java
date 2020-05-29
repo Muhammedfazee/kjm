88c2a2d32e24e2a3ee9b54a3777832a06fcdc540
Muhammedfazil.k <muhammedfazil.k@in-Muhammedfazilk.local>
2020-05-29 20:08:04 +0530

    initial commit

---
+++ src/main/java/com/kjmclub/KjmMemberManagementApplication.java
@@ -0,0 +1,13 @@
+package com.kjmclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KjmMemberManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KjmMemberManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kjmclub/KjmMemberManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjmclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KjmMemberManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
